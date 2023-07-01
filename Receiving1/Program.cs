468d7e24d4aa446525c5b3c9b629ce5ac352056c
Michele Corazza <info@michelecorazza.name>
2023-07-01 14:19:00 +0200

    Aggiungere i file di progetto.

---
+++ Receiving1/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Receiving 1!");
+
+        ConnectionFactory factory = new() { HostName = "localhost" };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            Console.WriteLine("Code name:");
+            var queueName = Console.ReadLine();
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            EventingBasicConsumer consumer = new(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                if (message.Contains("1"))
+                    Thread.Sleep(5000);
+
+                Console.WriteLine($"{DateTime.Now} [x] Received {message}");
+            };
+
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Press key to exit"); ;
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Sender!");
+
+        ConnectionFactory factory = new() { HostName = "localhost" };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            Console.WriteLine("Routing key:");
+            var routing = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Message:");
+                string? message = Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes(message ?? "not valid message");
+
+                channel.BasicPublish(
+                        exchange: "",
+                        routingKey: routing,
+                        basicProperties: null,
+                        body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+            }
+        }
+
+
+    }
+}
